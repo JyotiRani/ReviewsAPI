6f1f2216f2bf3d43b0c34bdd1f6878a040860228
Your Name <jyotirani10@in.ibm.com>
2017-06-14 14:04:29 +0530

    added validation

--- common/models/reviews.js
+++ common/models/reviews.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Reviews) {
+	
+Reviews.validatesNumericalityOf('rating', {int: true});
+Reviews.validatesLengthOf('remarks', { max: 500, message: { max: 'Remarks should not be more than 500 characters.' } });	
 
 };
