16a1bbad02b8fa586b89073baafd30a937beb063
Your Name <jyotirani10@in.ibm.com>
2017-06-20 11:15:19 +0530

    Added workItemType and CreatedDate fields

--- common/models/reviews.js
+++ common/models/reviews.js
@@ -3,6 +3,18 @@
 module.exports = function(Reviews) {
 	
 Reviews.validatesNumericalityOf('rating', {int: true});
-Reviews.validatesLengthOf('remarks', { max: 500, message: { max: 'Remarks should not be more than 500 characters.' } });	
+Reviews.validatesLengthOf('remarks', { max: 500, message: { max: 'Remarks should not be more than 500 characters.' } });
+
+Reviews.observe('before save', function updateTimestamp(ctx, next) {
+	  console.log('> before save triggered:', ctx.Model.modelName, ctx.instance ||
+      ctx.data);
+
+	var data = ctx.instance || ctx.data;  
+	if(ctx.isNewInstance) {
+		console.log('new instance setting created date');
+		data.createdDate = new Date();
+	}
+	next();
+});	
 
 };
